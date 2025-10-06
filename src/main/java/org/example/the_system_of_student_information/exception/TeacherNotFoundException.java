f5808ca0d28ab47ae26fa4a1ec23df451b2265ce
11Murad <ashrafov.main@gmail.com>
2025-10-07 02:08:08 +0400

    Relations was updated.

---
+++ src/main/java/org/example/the_system_of_student_information/exception/TeacherNotFoundException.java
@@ -0,0 +1,9 @@
+package org.example.the_system_of_student_information.exception;
+
+
+
+public class TeacherNotFoundException extends Exception{
+    public TeacherNotFoundException(String message) {
+            super(message);
+        }
+}
